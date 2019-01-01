33bffbfc7a7234b56a9e0927bf6482f0e701b34e
Carlos Casares <ccasares@Carloss-MacBook-Pro.local>
2019-01-01 13:17:00 +0100

    fix

--- schemas.js
+++ schemas.js
@@ -1,182 +1,21 @@
 module.exports = {
-
-  BOOKING: {
-    demozone: { presence: true },
-    "booking.bookingID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "booking.hotelID": { presence: true },
-    "booking.hotelName": { presence: true },
-    "booking.hotelBrand": { presence: true },
-    "booking.hotelCountry": { presence: true },
-    "booking.checkin": { presence: true },
-    "booking.checkout": { presence: true },
-    "customer.customerID": { presence: true },
-    "customer.socialID": { presence: true },
-    "customer.name": { presence: true },
-    "customer.surname": { presence: true },
-    "customer.age": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "customer.points": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } }
-  },
-  PRECHECKINREQUEST: {
-    demozone: { presence: true },
-    "booking.bookingID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "booking.hotelID": { presence: true },
-    "booking.hotelName": { presence: true },
-    "booking.hotelBrand": { presence: true },
-    "booking.hotelCountry": { presence: true },
-    "customer.customerID": { presence: true },
-    "customer.socialID": { presence: true },
-    "customer.name": { presence: true },
-    "customer.surname": { presence: true },
-    "customer.age": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } }
-  },
-  CHECKIN: {
-    demozone: { presence: true },
-    "booking.bookingID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "booking.hotelID": { presence: true },
-    "booking.hotelName": { presence: true },
-    "booking.hotelBrand": { presence: true },
-    "booking.hotelCountry": { presence: true },
-    "booking.checkindate": { presence: true },
-    "booking.roomID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "customer.customerID": { presence: true },
-    "customer.socialID": { presence: true },
-    "customer.name": { presence: true },
-    "customer.surname": { presence: true },
-    "customer.age": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "checkin.timestamp": { presence: true },
-    "checkin.gender": { presence: true },
-    "checkin.mood": { presence: true }
-  },
-  DOOROPENREQUEST: {
-    demozone: { presence: true },
-    "booking.bookingID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "booking.hotelID": { presence: true },
-    "booking.roomID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "customer.customerID": { presence: true }
-  },
-  TEMPCHANGEREQUEST: {
-    demozone: { presence: true },
-    "booking.bookingID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "booking.hotelID": { presence: true },
-    "booking.roomID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "customer.customerID": { presence: true },
-    "room.currentTemperature": { presence: true, numericality: { onlyInteger: false, greaterThan: 0, noStrings: true } },
-    "room.targetTemperature": { presence: true, numericality: { onlyInteger: false, greaterThan: 0, noStrings: true } }
-  },
-  PURCHASESERVICE: {
-    demozone: { presence: true },
-    "booking.bookingID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "booking.hotelID": { presence: true },
-    "booking.roomID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "customer.customerID": { presence: true },
-    "service.serviceID": { presence: true },
-    "service.name": { presence: true },
-    "service.cost": { presence: true, numericality: { onlyInteger: false, greaterThan: 0, noStrings: true } },
-    "service.points": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } }
-  },
-  MALFUNCTION: {
-    demozone: { presence: true },
-    "booking.bookingID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "booking.hotelID": { presence: true },
-    "booking.roomID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "customer.customerID": { presence: true },
-    "malfunction.element": { presence: true },
-    "malfunction.issue": { presence: true }
-  },
-  COZMODISPATCH: {
-    demozone: { presence: true },
-    "cozmo.request": { presence: true },
-    "cozmo.action": { presence: true },
-    "cozmo.parameters": { presence: false }
-  },
-  COZMOCOMPLETE: {
-    demozone: { presence: true },
-    "cozmo.request": { presence: true },
-    "cozmo.action": { presence: true },
-    "cozmo.parameters": { presence: false },
-    "cozmo.result": { presence: true },
-    "cozmo.timestamp": { presence: true }
-  },
-  CHECKOUT: {
-    demozone: { presence: true },
-    "booking.bookingID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "booking.hotelID": { presence: true },
-    "booking.hotelName": { presence: true },
-    "booking.hotelBrand": { presence: true },
-    "booking.hotelCountry": { presence: true },
-    "booking.checkindate": { presence: true },
-    "booking.checkoutdate": { presence: true },
-    "booking.roomID": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
-    "customer.customerID": { presence: true },
-    "customer.socialID": { presence: true },
-    "customer.name": { presence: true },
-    "customer.surname": { presence: true },
-    "customer.age": { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } }
-  },
-  SURVEY: {
-
+  PURCHASE: {
+    username: { presence: true },
+    timestamp: { presence: true },
+    product: { presence: true },
+    qty: { presence: true, numericality: { onlyInteger: true, greaterThan: 0, noStrings: true } },
+    txid: { presence: true },
+    price_total: { presence: true, numericality: { onlyInteger: false, greaterThan: 0, noStrings: true } }
   },
   KAFKAINBOUNDFORMAT: {
-    csv:  ['customer.customerID','booking.bookingID','timestamp','type','demozone','booking.roomID','checkin.timestamp','checkin.mood','checkin.gender','checkin.temperature','shower.roomID','shower.timestamp','shower.flow','shower.temp','temp.roomID','temp.timestamp','noise.roomID','noise.timestamp','noise.decibel','checkout.roomID','checkout.timestamp','checkout.mood','extension.roomID','extension.timestamp','extension.data1','extension.data2','extension.data3','extension.data4','extension.data5'],
+    csv:  ['username','timestamp','product','qty','txid','price_total'],
     json: {
-      demozone: '',
+      username: '',
       timestamp: '',
-      type: '',
-      customer: {
-      	customerID: ''
-      },
-      booking: {
-      	bookingID: '',
-      	roomID: 0
-      },
-      checkin: {
-      	timestamp: '',
-      	mood: '',
-      	gender: '',
-      	temperature: 0.0
-      },
-      shower: {
-      	roomID: '',
-      	timestamp: '',
-      	flow: '',
-      	temp: 0.0
-      },
-      temp: {
-      	roomID: '',
-      	timestamp: ''
-      },
-      noise: {
-      	roomID: '',
-      	timestamp: '',
-      	decibel: 0.0
-      },
-      checkout: {
-      	roomID: '',
-      	timestamp: '',
-      	mood: ''
-      },
-      extension: {
-      	roomID: '',
-      	timestamp: '',
-      	data1: '',
-      	data2: '',
-      	data3: '',
-      	data4: '',
-      	data5: ''
-      }
-    }
-  },
-  KAFKAOUTBOUNDFORMAT: {
-    csv:  ['customerID','bookingID','timestamp','type','demozone','roomID','mood'],
-    json: {
-      customerID: '',
-      bookingID: 0,
-      timestamp: '',
-      type: 0,
-      demozone: '',
-      roomID: 0,
-      mood: 0
+      product: '',
+      qty: 0,
+      txid: '',
+      price_total: 0
     }
   }
-
 };
--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var restify = require('restify')
   , json2csv = require('json2csv')
   , kafka = require('kafka-node')
   , async = require('async')
-  , validate = require("validate.js")
   , _ = require('lodash')
   , QUEUE = require('block-queue')
   , log = require('npmlog-ts')
@@ -16,50 +15,22 @@ var restify = require('restify')
   , getUsage = require('command-line-usage')
   , Schemas = require('./schemas')
   , util = require('util')
+  , uuidv4 = require('uuid/v4')
 ;
 
 // Instantiate classes & servers
-const wsURI        = '/socket.io'
-    , restURI      = '/wh/event/:eventname'
-    , sensorURI    = '/wh/sensor'
+const restURI      = '/kafka/send/:username/:product/:qty/:price'
     , CONNECTED    = "CONNECTED"
     , DISCONNECTED = "DISCONNECTED"
+    , RESTPORT     = 22222
 ;
 var restapp        = express()
   , restserver     = http.createServer(restapp)
   , Producer       = kafka.Producer
-  , Consumer       = kafka.Consumer
   , kafkaClient    = _.noop()
   , kafkaProducer  = _.noop()
-  , kafkaConsumer  = _.noop()
   , kafkaCnxStatus = DISCONNECTED;
 ;
-
-const EVENTS = [
-  { event: 'BOOKING', schema: Schemas.BOOKING, kafka: false },
-  { event: 'PRECHECKINREQUEST', schema: Schemas.PRECHECKINREQUEST, kafka: false },
-  { event: 'CHECKIN', schema: Schemas.CHECKIN, kafka: true },
-  { event: 'DOOROPENREQUEST', schema: Schemas.DOOROPENREQUEST, kafka: false },
-  { event: 'TEMPCHANGEREQUEST', schema: Schemas.TEMPCHANGEREQUEST, kafka: false },
-  { event: 'PURCHASESERVICE', schema: Schemas.PURCHASESERVICE, kafka: false },
-  { event: 'MALFUNCTION', schema: Schemas.MALFUNCTION, kafka: false },
-  { event: 'COZMODISPATCH', schema: Schemas.COZMODISPATCH, kafka: false },
-  { event: 'COZMOCOMPLETE', schema: Schemas.COZMOCOMPLETE, kafka: false },
-  { event: 'CHECKOUT', schema: Schemas.CHECKOUT, kafka: true }
-];
-
-const MOOD = [
-  { mood: "VERY ANGRY", code: 1},
-  { mood: "ANGRY", code: 2 },
-  { mood: "NEUTRAL", code: 3 },
-  { mood: "HAPPY", code: 4 },
-  { mood: "VERY HAPPY", code: 5 }
-];
-
-const GENDER = [
-  { gender: "MALE", code: 1},
-  { gender: "FEMALE", code: 2}
-];
 
 // ************************************************************************
 // Main code STARTS HERE !!
@@ -84,45 +55,20 @@ process.on('SIGINT', function() {
 
 // Initialize input arguments
 const optionDefinitions = [
-  { name: 'dbhost', alias: 'd', type: String },
-  { name: 'pinginterval', alias: 'n', type: Number },
-  { name: 'pingtimeout', alias: 't', type: Number },
   { name: 'zookeeperhost', alias: 'z', type: String },
   { name: 'kafkainboundtopic', alias: 'i', type: String },
-  { name: 'kafkaoutboundtopic', alias: 'o', type: String },
   { name: 'help', alias: 'h', type: Boolean },
   { name: 'verbose', alias: 'v', type: Boolean, defaultOption: false }
 ];
 
 const sections = [
   {
-    header: 'WEDO Hospitality - Event Server',
-    content: 'Event Server for WEDO Hospitality events'
+    header: 'Kafka Wrapper',
+    content: 'Kafka wrapper to send messages through REST API'
   },
   {
     header: 'Options',
     optionList: [
-      {
-        name: 'dbhost',
-        typeLabel: '[underline]{ipaddress:port}',
-        alias: 'd',
-        type: String,
-        description: 'DB setup server IP address/hostname and port'
-      },
-      {
-        name: 'pinginterval',
-        typeLabel: '[underline]{milliseconds}',
-        alias: 'n',
-        type: Number,
-        description: 'Ping interval in milliseconds for WS event clients'
-      },
-      {
-        name: 'pingtimeout',
-        typeLabel: '[underline]{milliseconds}',
-        alias: 't',
-        type: Number,
-        description: 'Ping timeout in milliseconds for WS event clients'
-      },
       {
         name: 'zookeeperhost',
         typeLabel: '[underline]{ipaddress:port}',
@@ -136,13 +82,6 @@ const sections = [
         alias: 'i',
         type: String,
         description: 'Kafka topic to send the events to'
-      },
-      {
-        name: 'kafkaoutboundtopic',
-        typeLabel: '[underline]{topic}',
-        alias: 'o',
-        type: String,
-        description: 'Kafka topic to receive the events from'
       },
       {
         name: 'verbose',
@@ -167,7 +106,7 @@ try {
   process.exit(-1);
 }
 
-if (!options.dbhost || !options.zookeeperhost || !options.kafkainboundtopic || !options.kafkaoutboundtopic) {
+if (!options.zookeeperhost || !options.kafkainboundtopic ) {
   console.log(getUsage(sections));
   process.exit(-1);
 }
@@ -179,28 +118,10 @@ if (options.help) {
 
 log.level = (options.verbose) ? 'verbose' : 'info';
 
-const pingInterval = options.pinginterval || 25000
-    , pingTimeout  = options.pingtimeout  || 60000
-    , RESTPORT = 20000
-    , DEMOZONESURI  = '/ords/pdb1/smarthospitality/demozone/zone'
-    , INSERTMOODURI = '/ords/pdb1/smarthospitality/mood/%s/%s'
-    , SHOWERURN     = 'urn:com:oracle:iot:device:shower_unit:attributes'
-    , NOISEURN      = 'urn:com:oracle:iot:device:noise_sensor:attributes'
-;
-
 // REST engine initial setup
 restapp.use(bodyParser.urlencoded({ extended: true }));
 restapp.use(bodyParser.json());
 
-var client = restify.createJsonClient({
-  url: 'https://' + options.dbhost,
-  rejectUnauthorized: false,
-  headers: {
-    "content-type": "application/json"
-  }
-});
-
-var demozones = _.noop();
 var servers = [];
 
 // Initializing QUEUE variables BEGIN
@@ -211,7 +132,7 @@ var inboundQueue  = []
 // Initializing QUEUE variables END
 
 function startKafka(cb) {
-  kafkaClient = new kafka.Client(options.zookeeperhost, "WEDO", {sessionTimeout: 1000});
+  kafkaClient = new kafka.Client(options.zookeeperhost, "RETAIL", {sessionTimeout: 1000});
   kafkaClient.zk.client.on('connected', () => {
     kafkaCnxStatus = CONNECTED;
     log.verbose("", "[Kafka] Server connected!");
@@ -225,12 +146,6 @@ function startKafka(cb) {
     log.verbose("", "[Kafka] Server disconnected!");
   });
   kafkaProducer = new Producer(kafkaClient);
-  kafkaConsumer = new Consumer(kafkaClient,
-    [
-      { topic: options.kafkaoutboundtopic, partition: 0 },
-      { topic: options.kafkaoutboundtopic, partition: 1 }
-    ]
-  );
   kafkaProducer.on('ready', () => {
     log.info("", "[Kafka] Producer ready");
     if (inboundQueue.length > 0) {
@@ -260,13 +175,6 @@ function startKafka(cb) {
   kafkaProducer.on('error', (err) => {
     log.error("", "Error initializing KAFKA producer: " + err.message);
   });
-  kafkaConsumer.on('ready', () => {
-    log.info("", "[Kafka] Consumer ready");
-  });
-  kafkaConsumer.on('message', (message) => {
-    log.verbose("", "[Kafka] Message received: " + JSON.stringify(message));
-    outboundQueue.push(message);
-  });
   if (typeof(cb) == 'function') cb(null);
 }
 
@@ -281,21 +189,6 @@ function stopKafka(cb) {
 }
 
 async.series([
-    function(next) {
-      // Get all demozones
-      log.verbose("", "Getting available demozones...");
-      client.get(DEMOZONESURI, function(err, req, res, obj) {
-        var jBody = JSON.parse(res.body);
-        if (err) {
-          next(err.message);
-        } else if (!jBody.items || jBody.items.length == 0) {
-          next("No demozones found. Aborting.");
-        } else {
-          demozones = jBody.items;
-          next(null);
-        }
-      });
-    },
     function(next) {
       // Initialize QUEUE system
       outboundQueue = QUEUE(queueConcurrency, function(message, done) {
@@ -342,38 +235,6 @@ async.series([
       next(null);
     },
     function(next) {
-      // Open a WS server for each demozone's baseport
-      async.eachSeries(demozones, (demozone,callback) => {
-        var d = {
-          demozone: demozone.id,
-          name: demozone.name,
-          port: parseInt("2" + demozone.baseport + "00")
-        };
-        d.app = express();
-        d.server = http.createServer(d.app);
-        d.io = require('socket.io')(d.server, {'pingInterval': pingInterval, 'pingTimeout': pingTimeout});
-        d.io.on('connection', function (socket) {
-          log.info(d.name,"Connected!!");
-          socket.conn.on('heartbeat', function() {
-            log.verbose(d.name,'heartbeat');
-          });
-          socket.on('disconnect', function () {
-            log.info(d.name,"Socket disconnected");
-          });
-          socket.on('error', function (err) {
-            log.error(d.name,"Error: " + err);
-          });
-        });
-        d.server.listen(d.port, function() {
-          log.info("","Created WS server for demozone '" + d.name + "' at port: " + d.port);
-          servers.push(d);
-          callback(null);
-        });
-      }, function(err) {
-        next(null);
-      });
-    },
-    function(next) {
       // Initialize KAFKA producer
       log.verbose("", "[Kafka] Connecting to Zookeper host at %s...", options.zookeeperhost);
       startKafka(next);
@@ -382,10 +243,6 @@ async.series([
       // Start REST server
       restserver.listen(RESTPORT, function() {
         log.info("","REST server running on http://localhost:" + RESTPORT + restURI);
-        log.info("","REST server running on http://localhost:" + RESTPORT + sensorURI);
-        log.verbose("", "Available events:%s", _.reduce(EVENTS, (str, e) => {
-          return str + " " + e.event;
-        }, ""));
         next(null);
       });
     }
@@ -396,135 +253,35 @@ async.series([
   }
 });
 
-restapp.post(restURI, function(req,res) {
-  if (req.params.eventname) {
-    var eventName = req.params.eventname.toUpperCase();
-    var event = _.find(EVENTS, ['event', eventName ]);
-    var payload = req.body;
-    if (!event || !payload) {
-      res.status(405).end();
-      return;
-    }
-    log.verbose("","Incoming request for event %s: %j", req.params.eventname, payload);
-    var assert = validate(payload, event.schema);
-    if (assert) {
-      res.status(405).end(assert);
-      return;
-    }
-    // Known event with valid schema, move on...
-    // First, send over WebSockets
-    var server = _.find(servers, { 'demozone': payload.demozone });
-    if (server) {
-      var namespace = req.params.eventname.toLowerCase();
-      log.verbose("","[WS] Sending event to %s (%s, %d)", namespace, payload.demozone, server.port);
-      server.io.sockets.emit(namespace, req.body);
-    } else {
-      log.error("", "Request received for a demozone not registered (" + payload.demozone + ")");
-    }
-    // Second, publish it to Kafka topic
-    if (event.kafka) {
-      log.verbose("","[Kafka] Sending %s event to %s", eventName, options.kafkainboundtopic);
-      var csvSchema = _.cloneDeep(Schemas.KAFKAINBOUNDFORMAT.json);
-      csvSchema.demozone = payload.demozone;
-      csvSchema.timestamp = new Date();
-      switch(event.event) {
-        case 'CHECKIN':
-          csvSchema.type = 1;
-          csvSchema.customer.customerID = payload.customer.customerID;
-          csvSchema.booking.bookingID = payload.booking.bookingID;
-          csvSchema.booking.roomID = payload.booking.roomID;
-          csvSchema.checkin.timestamp = new Date();
-          var m = _.find(MOOD, ['mood', payload.checkin.mood.toUpperCase() ]);
-          csvSchema.checkin.mood = (m) ? m.code : -1;
-          var g = _.find(GENDER, ['gender', payload.checkin.gender.toUpperCase() ]);
-          csvSchema.checkin.gender = (g) ? g.code : 3;
-          csvSchema.checkin.temperature = -1; // TODO
-        break;
-        case 'CHECKOUT':
-          csvSchema.type = 9;
-          csvSchema.customer.customerID = payload.customer.customerID;
-          csvSchema.booking.bookingID = payload.booking.bookingID;
-          csvSchema.checkout.roomID = payload.booking.roomID;
-          csvSchema.checkout.timestamp = new Date();
-          csvSchema.checkout.mood = ''; // TODO
-        break;
-      }
-      var csv = json2csv({ data: csvSchema, fields: Schemas.KAFKAINBOUNDFORMAT.csv, hasCSVColumnTitle: false, quotes: '' });
-      log.verbose("","[Kafka] CSV payload: %s", csv);
-      if (kafkaCnxStatus !== CONNECTED || !kafkaProducer) {
-        // Zookeeper connection lost, let's try to reconnect before giving up
+restapp.get(restURI, function(req,res) {
+  res.status(204).end();
+  log.verbose("","Incoming request for event with data: %s, %s, %s, %s", req.params.username, req.params.product, req.params.qty, req.params.price);
+  var csvSchema = _.cloneDeep(Schemas.KAFKAINBOUNDFORMAT.json);
+  csvSchema.username = req.params.username;
+  csvSchema.timestamp = new Date();
+  csvSchema.product = req.params.product;
+  csvSchema.qty = req.params.qty;
+  csvSchema.txid = uuidv4();
+  csvSchema.price_total = req.params.price;
+  var csv = json2csv({ data: csvSchema, fields: Schemas.KAFKAINBOUNDFORMAT.csv, hasCSVColumnTitle: false, quotes: '' });
+  log.verbose("","[Kafka] CSV payload: %s", csv);
+  if (kafkaCnxStatus !== CONNECTED || !kafkaProducer) {
+    // Zookeeper connection lost, let's try to reconnect before giving up
+    log.verbose("","[Kafka] Server not available. Enqueueing message");
+    inboundQueue.push(csv);
+    log.verbose("","[Kafka] Trying to reconnect to Kafka server...");
+    stopKafka(() => {
+      log.verbose("","[Kafka] Kafka Object closed");
+      startKafka();
+    });
+  } else {
+    kafkaProducer.send([{ topic: options.kafkainboundtopic, messages: csv, partition: 0 }], (err, data) => {
+      if (err) {
+        log.error("", err);
         log.verbose("","[Kafka] Server not available. Enqueueing message");
         inboundQueue.push(csv);
-        log.verbose("","[Kafka] Trying to reconnect to Kafka server...");
-        stopKafka(() => {
-          log.verbose("","[Kafka] Kafka Object closed");
-          startKafka();
-        });
       } else {
-        kafkaProducer.send([{ topic: options.kafkainboundtopic, messages: csv, partition: 0 }], (err, data) => {
-          if (err) {
-            log.error("", err);
-            log.verbose("","[Kafka] Server not available. Enqueueing message");
-            inboundQueue.push(csv);
-          } else {
-            log.verbose("", "[Kafka] Message sent to topic %s, partition %s and id %d", Object.keys(data)[0], Object.keys(Object.keys(data)[0])[0], data[Object.keys(data)[0]][Object.keys(Object.keys(data)[0])[0]]);
-          }
-        });
-      }
-    }
-    res.status(204).end();
-  }
-});
-
-restapp.post(sensorURI, function(req,res) {
-  res.status(200).end();
-  // Sanity checks
-  if ( !req.body) {
-    return;
-  }
-  if (Array.isArray(req.body)) {
-    _.forEach(req.body, (e) => {
-      if (e.payload && e.payload.format && e.payload.data) {
-        // Everything seems ok
-        var csvSchema = _.cloneDeep(Schemas.KAFKAINBOUNDFORMAT.json);
-        csvSchema.demozone = e.payload.data.demo_zone;
-        csvSchema.timestamp = new Date();
-        if (e.payload.format === SHOWERURN) {
-          csvSchema.type = 2;
-          csvSchema.shower.roomID = e.payload.data.hotel_room;
-          csvSchema.shower.timestamp = new Date();
-          csvSchema.shower.flow = e.payload.data.water_flow;
-          csvSchema.shower.temp = e.payload.data.water_temp;
-        } else if (e.payload.format === NOISEURN) {
-          csvSchema.type = 3;
-          csvSchema.noise.roomID = e.payload.data.hotel_room;
-          csvSchema.noise.timestamp = new Date();
-          csvSchema.noise.decibel = e.payload.data.noise;
-        } else {
-          return;
-        }
-        var csv = json2csv({ data: csvSchema, fields: Schemas.KAFKAINBOUNDFORMAT.csv, hasCSVColumnTitle: false, quotes: '' });
-        log.verbose("","[Kafka] CSV payload: %s", csv);
-        if (kafkaCnxStatus !== CONNECTED || !kafkaProducer) {
-          // Zookeeper connection lost, let's try to reconnect before giving up
-          log.verbose("","[Kafka] Server not available. Enqueueing message");
-          inboundQueue.push(csv);
-          log.verbose("","[Kafka] Trying to reconnect to Kafka server...");
-          stopKafka(() => {
-            log.verbose("","[Kafka] Kafka Object closed");
-            startKafka();
-          });
-        } else {
-          kafkaProducer.send([{ topic: options.kafkainboundtopic, messages: csv, partition: 0 }], (err, data) => {
-            if (err) {
-              log.error("", err);
-              log.verbose("","[Kafka] Server not available. Enqueueing message");
-              inboundQueue.push(csv);
-            } else {
-              log.verbose("", "[Kafka] Message sent to topic %s, partition %s and id %d", Object.keys(data)[0], Object.keys(Object.keys(data)[0])[0], data[Object.keys(data)[0]][Object.keys(Object.keys(data)[0])[0]]);
-            }
-          });
-        }
+        log.verbose("", "[Kafka] Message sent to topic %s, partition %s and id %d", Object.keys(data)[0], Object.keys(Object.keys(data)[0])[0], data[Object.keys(data)[0]][Object.keys(Object.keys(data)[0])[0]]);
       }
     });
   }
